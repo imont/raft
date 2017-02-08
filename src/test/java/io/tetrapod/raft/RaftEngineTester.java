abc1c6b3440b520255a832e09003c4b26a573601
Romanas Sonkinas <romanass@gmail.com>
2017-02-08 13:49:05 +0000

    More tests

--- src/test/java/io/tetrapod/raft/RaftEngineTester.java
+++ src/test/java/io/tetrapod/raft/RaftEngineTester.java
@@ -3,17 +3,13 @@ package io.tetrapod.raft;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Ignore;
-import org.junit.Test;
+import org.junit.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.security.SecureRandom;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Callable;
@@ -22,6 +18,11 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import static com.jayway.awaitility.Awaitility.await;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNot.not;
 
 /**
  * Runs a full system simulation with fake RPC
@@ -34,8 +35,8 @@ public class RaftEngineTester implements RaftRPC<TestStateMachine> {
     private static ScheduledExecutorService executor;
     private static File[] logDirs = new File[NUM_PEERS];
 
-    @BeforeClass
-    public static void makeTestDir() throws IOException {
+    @Before
+    public void createTestDirectories() throws IOException {
         for (int i = 0; i < NUM_PEERS; i++) {
             File parent = Files.createTempDirectory("raft").toFile();
             logDirs[i] = new File(parent, "logs/test-" + (i + 1));
@@ -46,14 +47,8 @@ public class RaftEngineTester implements RaftRPC<TestStateMachine> {
         executor = Executors.newScheduledThreadPool(NUM_PEERS);
     }
 
-    @AfterClass
-    public static void deleteTestDir() {
-        //      for (int i = 0; i <  NUM_PEERS; i++) {
-        //         for (File file : logDirs[i].listFiles()) {
-        //            file.delete();
-        //         }
-        //         logDirs[i].delete();
-        //      }
+    @After
+    public void shutdown() {
         executor.shutdownNow();
     }
 
@@ -62,19 +57,90 @@ public class RaftEngineTester implements RaftRPC<TestStateMachine> {
     }
 
     private Map<Integer, RaftEngine<TestStateMachine>> rafts = new HashMap<>();
-    private SecureRandom random = new SecureRandom();
 
     @Test
     public void willElectLeaderInitially() throws Exception {
         // given
-        for (int i = 1; i <= NUM_PEERS; i++) {
+        startEverything(3);
+
+        // when
+        // it's running
+
+        // then
+        assertLeaderElected();
+    }
+
+    @Ignore("WIP")
+    @Test
+    public void willElectNewLeaderWhenCurrentOneLeavesCluster() throws IOException {
+
+    }
+
+    @Test
+    public void willElectNewLeaderWhenCurrentOneDisconnectsTemporarily() throws IOException {
+        // given
+        startEverything(3);
+        assertLeaderElected();
+        RaftEngine<TestStateMachine> leader = getLeader();
+
+        // when
+        // it's running
+        leader.stop();
+
+        // then
+        assertLeaderElected();
+        RaftEngine<TestStateMachine> newLeader = getLeader();
+        assertThat(leader.getPeerId(), not(equalTo(newLeader.getPeerId())));
+    }
+
+    @Ignore("WIP")
+    @Test
+    public void willNotElectLeaderIfNotQuorum() throws Exception {
+        // given
+        startEverything(3);
+        assertLeaderElected();
+        RaftEngine<TestStateMachine> leader = getLeader();
+        RaftEngine<TestStateMachine> follower = getFirstFollower();
+
+        // when
+        // it's running
+        leader.stop();
+        follower.stop();
+
+        // then
+        Thread.sleep(1000);
+        assertThat(getLeader(), is(nullValue()));
+    }
+
+    private RaftEngine<TestStateMachine> getLeader() {
+        for (Map.Entry<Integer, RaftEngine<TestStateMachine>> entry : rafts.entrySet()) {
+            if (entry.getValue().getRole() == RaftEngine.Role.Leader) {
+                return entry.getValue();
+            }
+        }
+        return null;
+    }
+
+    private RaftEngine<TestStateMachine> getFirstFollower() {
+        for (Map.Entry<Integer, RaftEngine<TestStateMachine>> entry : rafts.entrySet()) {
+            if (entry.getValue().getRole() == RaftEngine.Role.Follower) {
+                return entry.getValue();
+            }
+        }
+        return null;
+    }
+
+
+    private void startEverything(int numPeers) throws IOException {
+        // given
+        for (int i = 1; i <= numPeers; i++) {
             Config cfg = new Config()
                     .setLogDir(logDirs[i - 1])
                     .setClusterName("TEST")
                     .setElectionTimeoutFixedMillis(100);
             RaftEngine<TestStateMachine> raft = new RaftEngine<TestStateMachine>(cfg, new TestStateMachine.Factory(), this);
             raft.setPeerId(i);
-            for (int j = 1; j <= NUM_PEERS; j++) {
+            for (int j = 1; j <= numPeers; j++) {
                 if (j != i) {
                     raft.addPeer(j);
                 }
@@ -86,8 +152,9 @@ public class RaftEngineTester implements RaftRPC<TestStateMachine> {
         for (RaftEngine<?> raft : rafts.values()) {
             raft.start(raft.getPeerId());
         }
+    }
 
-        // then
+    private void assertLeaderElected() {
         await().atMost(5, TimeUnit.SECONDS).until(new Callable<Map<Integer, RaftEngine<TestStateMachine>>>() {
             @Override
             public Map<Integer, RaftEngine<TestStateMachine>> call() throws Exception {
@@ -96,29 +163,11 @@ public class RaftEngineTester implements RaftRPC<TestStateMachine> {
         }, leaderElected());
     }
 
-    @Ignore("WIP")
-    @Test
-    public void willElectNewLeaderWhenCurrentOneLeavesCluster() {
-
-    }
-
-    @Ignore("WIP")
-    @Test
-    public void willElectNewLeaderWhenCurrentOneDisconnectsTemporarily() {
-
-    }
-
-    @Ignore("WIP")
-    @Test
-    public void willNotElectLeaderIfNotQuorum() {
-
-    }
-
     @Override
     public void sendRequestVote(final String clusterName, int peerId, final long term, final int candidateId, final long lastLogIndex,
                                 final long lastLogTerm, final VoteResponseHandler handler) {
         final RaftEngine<?> r = rafts.get(peerId);
-        if (r != null) {
+        if (r != null && r.getRole() != RaftEngine.Role.Leaving) {
             executor.schedule(new Runnable() {
                 @Override
                 public void run() {
@@ -136,7 +185,7 @@ public class RaftEngineTester implements RaftRPC<TestStateMachine> {
     public void sendAppendEntries(int peerId, final long term, final int leaderId, final long prevLogIndex, final long prevLogTerm,
                                   final Entry<TestStateMachine>[] entries, final long leaderCommit, final AppendEntriesResponseHandler handler) {
         final RaftEngine<TestStateMachine> r = rafts.get(peerId);
-        if (r != null) {
+        if (r != null && r.getRole() != RaftEngine.Role.Leaving) {
             executor.schedule(new Runnable() {
                 @Override
                 public void run() {
@@ -154,7 +203,7 @@ public class RaftEngineTester implements RaftRPC<TestStateMachine> {
     public void sendInstallSnapshot(int peerId, final long term, final long index, final long length, final int partSize, final int part,
                                     final byte[] data, final InstallSnapshotResponseHandler handler) {
         final RaftEngine<TestStateMachine> r = rafts.get(peerId);
-        if (r != null) {
+        if (r != null && r.getRole() != RaftEngine.Role.Leaving) {
             executor.schedule(new Runnable() {
                 @Override
                 public void run() {
@@ -171,11 +220,15 @@ public class RaftEngineTester implements RaftRPC<TestStateMachine> {
     @Override
     public void sendIssueCommand(int peerId, final Command<TestStateMachine> command, final ClientResponseHandler<TestStateMachine> handler) {
         final RaftEngine<TestStateMachine> r = rafts.get(peerId);
-        if (r != null) {
+        if (r != null && r.getRole() != RaftEngine.Role.Leaving) {
             executor.schedule(new Runnable() {
                 @Override
                 public void run() {
-
+                    try {
+                        r.handleClientRequest(command, handler);
+                    } catch (Throwable t) {
+                        logger.error(t.getMessage(), t);
+                    }
                 }
             }, randomDelay(), TimeUnit.MILLISECONDS);
         }
